f210d05d081be2c3924430504ef6e6ba3e6985fc
Dyachkov Anton <dyachkov1984@inbox.ru>
2023-10-01 17:45:02 +0300

    commit # 1.1

--- Homework # 1/src/ModelElements/PoligonalModel.java
+++ Homework # 1/src/ModelElements/PoligonalModel.java
@@ -6,11 +6,24 @@ import java.util.List;
 import Stuff.Poligon;
 import Stuff.Texture;
 
-public class PoligonalModel {
-    public List<Poligon> poligons;
-    public List <Texture> textures;
+//public class PoligonalModel {
+    //public List<Poligon> poligons;
+    //public List <Texture> textures;
 
-    public PoligonalModel() {
-        this.poligons = new ArrayList<Poligon>();
+    //public PoligonalModel() {
+        //this.poligons = new ArrayList<Poligon>();
+    //}
+//}
+
+
+public class PoligonalModel {
+    public List<Texture> Textures;
+    public List<Poligon> Poligons;
+    
+    public PoligonalModel(List<Texture> Textures) {
+        this.Poligons = new ArrayList<>();
+        this.Textures = Textures;
+    
+        this.Poligons.add(new Poligon(new Point3D()));
     }
 }
--- Homework # 1/src/ModelElements/Scene.java
+++ Homework # 1/src/ModelElements/Scene.java
@@ -3,14 +3,51 @@ package ModelElements;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Scene {
-    public int id;
-    public List<PoligonalModel> models;
-    public List<Flash> flashes;
-    public List<Camera> cameras;
-    public Scene() {
-        this.models = new ArrayList<PoligonalModel>();
-        this.cameras = new ArrayList<Camera>();
-    }
+//public class Scene {
+    //public int id;
+    //public List<PoligonalModel> models;
+    //public List<Flash> flashes;
+    //public List<Camera> cameras;
+    //public Scene() {
+        //this.models = new ArrayList<PoligonalModel>();
+        //this.cameras = new ArrayList<Camera>();
+    //}
 //Реализовать методы
+//}
+
+public Scene(int id, List<PoligonalModel> models, List<Flash> flashes, List<Camera> cameras) throws Exception {
+    this.ID = id;
+    if (models.size() > 0) {
+        this.Models = models;
+    } else {
+        throw new Exception("Должна быть одна модель");
+    }
+    this.Flashes = flashes;
+    if (cameras.size() > 0) {
+        this.cameras = cameras;
+    } else {
+        throw new Exception("Должна быть одна камера");
+    }
 }
+
+public <T> T method1(T flash) {
+    return flash;
+}
+
+public <T, E> T method2(T Model, E Flash) {
+    return Model;
+}
+
+// /**
+// * заглушка
+// */
+// public Type1 method1(Type1 t) {
+// return t;
+// }
+
+// /**
+// * заглушка
+// */
+// public Type1 method1(Type1 t1, Type2 t2) {
+// return t1;
+// }
